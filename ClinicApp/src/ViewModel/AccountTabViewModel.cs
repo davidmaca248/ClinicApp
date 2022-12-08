a38ac44a591100bca172c2c7093a580fe7868ec4
unknown <mabozheng@gmail.com>
2022-12-07 23:06:03 -0700

    Added user login pages and password checking

--- ClinicApp/src/ViewModel/AccountTabViewModel.cs
+++ ClinicApp/src/ViewModel/AccountTabViewModel.cs
@@ -20,26 +20,54 @@ namespace ClinicApp.ViewModel
                 OnPropertyChanged();
             }
         }
+
+        public int currUser {
+            get => curruser;
+            set
+            {
+                curruser= value;
+            }
+        }
+
+        private int curruser;
         #endregion
 
         #region Members
         private object _currentContent;
         private AccountLoggedIn _AccountLoggedView;
         private AccountUC _AccountContentView;
+        private User1 user1;
+        private User2 user2;
+        private User3 user3;
+
         #endregion
         public AccountTabViewModel()
         {
             _AccountLoggedView = new AccountLoggedIn(this);
             _AccountContentView = new AccountUC(this);
-
+            user1 = new User1(this);
+            user2 = new User2(this);
+            user3 = new User3(this);
             CurrentAccountContent = _AccountContentView;
         }
         public void UserLoggedIn(int user)
         {
           
             _AccountLoggedView.ViewModel.UserLoggedIn = user;
-            
-            CurrentAccountContent = _AccountLoggedView;
+            curruser= user;
+            if(user == 1)
+            {
+                CurrentAccountContent = user1;
+
+            }
+            else if(user == 2)
+            {
+                CurrentAccountContent = user2;
+            }
+            else if(user == 3)
+            {
+                CurrentAccountContent = user3;
+            }
 
         }
 
