43635fbd7b4af45d846e86c7aeed61d328a1ca3e
david.macababayao <david.macababayao@cs.ucalgary.ca>
2022-12-11 14:57:31 -0700

    fixed name and appointments

--- ClinicApp/src/Globals/GlobalHomePageListDatabase.cs
+++ ClinicApp/src/Globals/GlobalHomePageListDatabase.cs
@@ -20,22 +20,18 @@ namespace ClinicApp.Globals
 
             NotesList = new List<NoteListItem>()
             {
-                new NoteListItem(1,"Arhum's test results came in last night", new DateTime(2022,12,day1,9,15,0)),
+                new NoteListItem(1,"Chris Ben's test results came in last night", new DateTime(2022,12,day2,9,15,0)),
                 new NoteListItem(2,"Alden Hoffman canceled appointment", new DateTime(2022,12,day2,9,15,0)),
                 new NoteListItem(3,"Dan called in sick", new DateTime(2022,12,day3,9,15,0)),
             };
 
             TodoList = new List<NoteListItem>()
             {
-                new NoteListItem(1,"Call David Maca to confirm appointment", new DateTime(2022,12,day1,9,15,0)),
-                new NoteListItem(2,"Let Dr. Frank know about Arhum's test results", new DateTime(2022,12,day2,9,15,0)),
+                new NoteListItem(1,"Call David Maca to confirm appointment", new DateTime(2022,12,day2,9,15,0)),
+                new NoteListItem(2,"Let Dr. Frank know about Chris's test results", new DateTime(2022,12,day2,9,15,0)),
                 new NoteListItem(3, "Let Dr. Josh know about Alden Hoffman's cancellation", new DateTime(2022,12,day3,9,15,0)),
                 new NoteListItem(4,"Let manager know that Dan cannot make it to work today", new DateTime(2022,11,day2,9,15,0)),
 
-                //new NoteListItem("Call David Maca to confirm appointment", new DateTime(2022,12,day2,23,15,0)),
-                //new NoteListItem("Let Dr. Frank know about Arhum's test results", new DateTime(2022,12,day2,22,45,0)),
-                //new NoteListItem("Let Dr. Josh know about Alden Hoffman's cancellation", new DateTime(2022,12,day2,23,15,0)),
-                //new NoteListItem("Let manager know that Dan cannot make it to work today", new DateTime(2022,11,day2,23,15,0)),
             };
         }
     }
